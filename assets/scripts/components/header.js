cbae88ed76e92df204b8b78205f490f6aa57c3dc
Endsunset <hutsunemiku_redstone@icloud.com>
2025-06-07 20:55:37 +0800

    Update header.js

--- assets/scripts/components/header.js
+++ assets/scripts/components/header.js
@@ -5,11 +5,11 @@ export function loadHeader() {
     fetchWithFallback(
         'https://endsunset.github.io/partials/header.html',
         'header-container',
-        '<div class="fallback">
+        `<div class="fallback">
             <a href="/">Endsunset</a> |
             <a href="/documentation/">Documentation</a>
             <a href="/videos/">Videos</a>
             <a href="/">Support</a>
-        </div>'
+        </div>`
     );
 }
