ff85f9caf5a02d0790fefd7b3faab11c105c4423
Endsunset <hutsunemiku_redstone@icloud.com>
2025-06-07 19:34:26 +0800

    Create main.js

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+import { loadHeader } from './components/header.js';
+import { loadFooter } from './components/footer.js';
+
+// Initialize components
+document.addEventListener('DOMContentLoaded', () => {
+    loadHeader();
+    loadFooter();
+});
